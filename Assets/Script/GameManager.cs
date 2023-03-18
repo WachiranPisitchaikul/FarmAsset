1400e534f86935063d0c6490cb486f3dcbcb4cca
WachiranPisitchaikul <pisitchaikulwachiratan@gmail.com>
2023-03-18 10:59:01 +0700

    farm basic

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject staffPrefab;
+    public GameObject staffParent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Generatecandidate();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void Generatecandidate()
+    {
+        for(int i = 0; i <20;i++)
+        {
+            GameObject staffObj = Instantiate(staffPrefab, staffParent.transform);
+            Staff s = staffObj.GetComponent<Staff>();
+
+            s.InitCharID(i);
+            s.ChangeCharSkin();
+        }
     }
 }
